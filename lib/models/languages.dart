fc25f13e57b5143374ca2e2251ba0557f97adfc0
OeungChheanghok123 <chheanghok44@gmail.com>
2022-05-06 22:44:53 +0700

    06 May

---
+++ lib/models/languages.dart
@@ -0,0 +1,37 @@
+import 'package:get/get.dart';
+
+class Languages extends Translations {
+
+  @override
+  Map<String, Map<String, String>> get keys => {
+
+    'en_US' : {
+      'Log In' : 'Log In',
+      'Become a Driver?' : 'Become a Driver?',
+      'Stats' : 'Stats',
+      'Breakdown' : 'Breakdown',
+      'Edit Profile' : 'Edit Profile',
+      'Rating Score' : 'Rating Score',
+      'Invite Friends to Earn Points' : 'Invite Friends to Earn Points',
+      'Need a Support' : 'Need a Support',
+      'Feedback Us' : 'Feedback Us',
+      'Language' : 'Language',
+      'Logout' : 'Logout',
+    },
+
+    'kh_KH' : {
+      'Log In' : 'ចូល',
+      'Become a Driver?' : 'ក្លាយជាអ្នកបើកបរ?',
+      'Stats' : 'ស្ថិតិ',
+      'Breakdown' : 'ពត៌មានបន្ថែម',
+      'Edit Profile' : 'កែសម្រួលប្រូហ្វាល',
+      'Rating Score' : 'ពិន្ទុវាយតម្លៃ',
+      'Invite Friends to Earn Points' : 'ណែនាំមិត្តភក្តិដើម្បីទទួលបានពិន្ទុ',
+      'Need a Support' : 'ត្រូវការជំនួយ',
+      'Feedback Us' : 'វាយតម្លៃយើង',
+      'Language' : 'កំណត់ភាសា',
+      'Logout' : 'ចាកចេញ',
+    },
+
+  };
+}
