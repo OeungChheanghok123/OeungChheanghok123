4d3c28cca2f64d773d033870f830a7086159ae16
OeungChheanghok123 <chheanghok44@gmail.com>
2022-10-23 18:25:46 +0700

    ddfs

--- lib/controllers/google_map_controller.dart
+++ lib/controllers/google_map_controller.dart
@@ -29,57 +29,81 @@ class MapController extends GetxController {
   List<LatLng> polylineCoordinates = [];
   PolylinePoints polylinePoints = PolylinePoints();
 
-  final _orderData = RemoteData<List<OrderModel>>(status: RemoteDataStatus.processing, data: null).obs;
+  final _orderData = RemoteData<List<OrderModel>>(
+          status: RemoteDataStatus.processing, data: null)
+      .obs;
   RemoteData<List<OrderModel>> get orderData => _orderData.value;
 
-  final _merchantData = RemoteData<List<MerchantModel>>(status: RemoteDataStatus.processing, data: null).obs;
+  final _merchantData = RemoteData<List<MerchantModel>>(
+          status: RemoteDataStatus.processing, data: null)
+      .obs;
   RemoteData<List<MerchantModel>> get merchantData => _merchantData.value;
 
-  final _customerData = RemoteData<List<CustomerModel>>(status: RemoteDataStatus.processing, data: null).obs;
+  final _customerData = RemoteData<List<CustomerModel>>(
+          status: RemoteDataStatus.processing, data: null)
+      .obs;
   RemoteData<List<CustomerModel>> get customerData => _customerData.value;
 
   @override
   void onInit() {
     super.onInit();
     getCurrentCameraPosition();
+    locationChange();
     loadOrderData();
+  }
+
+  void locationChange() {
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+    final positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) {
+      latitude.value = position!.latitude;
+      longitude.value = position.longitude;
+    });
   }
 
   Future<Position> getCurrentPosition() async {
     final permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.always || permission == LocationPermission.whileInUse) {
+    if (permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse) {
       return await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high,
       );
-    }
-    else {
+    } else {
       final request = await Geolocator.requestPermission();
-      if (request == LocationPermission.always || request == LocationPermission.whileInUse) {
+      if (request == LocationPermission.always ||
+          request == LocationPermission.whileInUse) {
         return await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.high,
         );
-      }
-      else {
+      } else {
         throw Exception("Access to Location permission was denied.");
       }
     }
   }
+
   Future<CameraPosition> getCurrentCameraPosition() async {
     Position _currentPos = await getCurrentPosition();
     latitude.value = _currentPos.latitude;
     longitude.value = _currentPos.longitude;
-    if (_currentPos != null) {        // ignore: unnecessary_null_comparison
+    if (_currentPos != null) {
+      // ignore: unnecessary_null_comparison
       return CameraPosition(
         target: LatLng(latitude.value, longitude.value),
         zoom: zoom.value,
       );
     } else {
-      throw Exception('Error current location because Google Maps permission was denied.');
+      throw Exception(
+          'Error current location because Google Maps permission was denied.');
     }
   }
 
   Future<Set<Marker>> initMerchantMarker() async {
-    final icon = BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue);
+    final icon =
+        BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue);
     final merchant = merchantData.data![0];
     for (LatLng location in myPosLatLng) {
       Marker marker = Marker(
@@ -93,6 +117,7 @@ class MapController extends GetxController {
     }
     return markers.toSet();
   }
+
   InfoWindow infoWindowMerchant(MerchantModel merchantModel) {
     return InfoWindow(
       title: merchantModel.merchantName,
@@ -101,7 +126,8 @@ class MapController extends GetxController {
   }
 
   Future<Set<Marker>> initCustomerMarker() async {
-    final icon = BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen);
+    final icon =
+        BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen);
     final customer = customerData.data![0];
     for (LatLng location in myPosLatLng) {
       Marker marker = Marker(
@@ -115,6 +141,7 @@ class MapController extends GetxController {
     }
     return markers.toSet();
   }
+
   InfoWindow infoWindowCustomer(CustomerModel customerModel) {
     return InfoWindow(
       title: customerModel.customerName,
@@ -124,11 +151,17 @@ class MapController extends GetxController {
 
   void loadOrderData() {
     try {
-      final data = FirebaseFirestore.instance.collection(OrderModel.collectionName).where(OrderModel.orderIdString, isEqualTo: newOrderController.newOrderId.value).snapshots();
+      final data = FirebaseFirestore.instance
+          .collection(OrderModel.collectionName)
+          .where(OrderModel.orderIdString,
+              isEqualTo: newOrderController.newOrderId.value)
+          .snapshots();
       data.listen((result) {
-        if (result.docs.isNotEmpty){
-          final orders = result.docs.map((e) => OrderModel.fromMap(e.data())).toList();
-          _orderData.value = RemoteData<List<OrderModel>>(status: RemoteDataStatus.success, data: orders);
+        if (result.docs.isNotEmpty) {
+          final orders =
+              result.docs.map((e) => OrderModel.fromMap(e.data())).toList();
+          _orderData.value = RemoteData<List<OrderModel>>(
+              status: RemoteDataStatus.success, data: orders);
 
           merchantId.value = orders[0].merchantId;
           customerId.value = orders[0].customerId;
@@ -136,41 +169,60 @@ class MapController extends GetxController {
         }
       });
     } catch (ex) {
-      _orderData.value = RemoteData<List<OrderModel>>(status: RemoteDataStatus.error, data: null);
+      _orderData.value = RemoteData<List<OrderModel>>(
+          status: RemoteDataStatus.error, data: null);
     }
   }
+
   void loadMerchantData() {
     try {
-      final data = FirebaseFirestore.instance.collection(MerchantModel.collectionName).where(MerchantModel.merchantIdString, isEqualTo: merchantId.value).snapshots();
+      final data = FirebaseFirestore.instance
+          .collection(MerchantModel.collectionName)
+          .where(MerchantModel.merchantIdString, isEqualTo: merchantId.value)
+          .snapshots();
       data.listen((result) {
-        final merchant = result.docs.map((e) => MerchantModel.fromMap(e.data())).toList();
-        _merchantData.value = RemoteData<List<MerchantModel>>(status: RemoteDataStatus.success, data: merchant);
+        final merchant =
+            result.docs.map((e) => MerchantModel.fromMap(e.data())).toList();
+        _merchantData.value = RemoteData<List<MerchantModel>>(
+            status: RemoteDataStatus.success, data: merchant);
 
-        merchantPosition = LatLng(merchant[0].position.latitude, merchant[0].position.longitude);
+        merchantPosition = LatLng(
+            merchant[0].position.latitude, merchant[0].position.longitude);
         myPosLatLng.add(merchantPosition);
-        initMerchantMarker().then((marker) => markers = marker as RxList<Marker>);
+        initMerchantMarker()
+            .then((marker) => markers = marker as RxList<Marker>);
         debugPrint('merchant position : $merchantPosition');
         getPolyline(merchantPosition);
       });
     } catch (ex) {
-      _merchantData.value = RemoteData<List<MerchantModel>>(status: RemoteDataStatus.error, data: null);
+      _merchantData.value = RemoteData<List<MerchantModel>>(
+          status: RemoteDataStatus.error, data: null);
     }
   }
+
   void loadCustomerData() {
     try {
-      final data = FirebaseFirestore.instance.collection(CustomerModel.collectionName).where(CustomerModel.customerIdString, isEqualTo: customerId.value).snapshots();
+      final data = FirebaseFirestore.instance
+          .collection(CustomerModel.collectionName)
+          .where(CustomerModel.customerIdString, isEqualTo: customerId.value)
+          .snapshots();
       data.listen((result) {
-        final customer = result.docs.map((e) => CustomerModel.fromMap(e.data())).toList();
-        _customerData.value = RemoteData<List<CustomerModel>>(status: RemoteDataStatus.success, data: customer);
+        final customer =
+            result.docs.map((e) => CustomerModel.fromMap(e.data())).toList();
+        _customerData.value = RemoteData<List<CustomerModel>>(
+            status: RemoteDataStatus.success, data: customer);
 
-        customerPosition = LatLng(customer[0].position.latitude, customer[0].position.longitude);
+        customerPosition = LatLng(
+            customer[0].position.latitude, customer[0].position.longitude);
         myPosLatLng.clear();
         myPosLatLng.add(customerPosition);
-        initCustomerMarker().then((marker) => markers = marker as RxList<Marker>);
+        initCustomerMarker()
+            .then((marker) => markers = marker as RxList<Marker>);
         getPolyline(customerPosition);
       });
     } catch (ex) {
-      _customerData.value = RemoteData<List<CustomerModel>>(status: RemoteDataStatus.error, data: null);
+      _customerData.value = RemoteData<List<CustomerModel>>(
+          status: RemoteDataStatus.error, data: null);
     }
   }
 
@@ -189,6 +241,7 @@ class MapController extends GetxController {
     }
     addPolyLine(location.toString());
   }
+
   void addPolyLine(String location) {
     PolylineId id = PolylineId(location);
     Polyline polyline = Polyline(
